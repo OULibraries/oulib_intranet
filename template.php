1303198b898b2ae08531f48339bccab4048a7c39
Twila Camp <tcamp@ou.edu>
2016-10-10 13:40:19 -0500

    fixing IE compatibility

--- template.php
+++ template.php
@@ -4,3 +4,20 @@
  * @file
  * template.php
  */
+
+/**
+* Implements hook_preprocess_html().
+*/
+function oulib_intranet_preprocess_html (&$vars) {
+  
+  $data = array(
+     '#tag' => 'meta',
+     '#attributes' => array(
+       'http-equiv' => "X-UA-Compatible",
+       'content' => "IE=edge",
+       )
+     );
+
+   drupal_add_html_head($data,'key');
+   
+ }
