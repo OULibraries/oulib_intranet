415fbc411766ccab29d300bf6b4d37288a2490c8
Tim Smith <timsmith@ou.edu>
2017-03-15 14:31:46 -0500

    hide main menu for anonymous users

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -119,7 +119,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <?php if (!empty($primary_nav)): ?>
+        <?php if (!empty($primary_nav) && user_is_logged_in()): ?>
             <?php print render($primary_nav); ?>
         <?php endif; ?>  
       </ul>   
